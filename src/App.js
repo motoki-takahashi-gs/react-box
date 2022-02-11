c1a279c1bbdaa87c6b225bf1d3a1391db46e466d
Motoki <report.japan.hiroshima@gmail.com>
2022-02-11 16:39:53 +0900

    Boxes with state managed by incoming props with imperative way

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import Box from './components/Box';
 import boxes from './data/boxes';
+import Box from './components/Box';
 import './App.css';
 
 function App() {
   const [squares, setSquares] = React.useState(boxes)
 
+  const toggle = (id) => {
+    setSquares(prevSquares => {
+      const newSquares = []
+      for (let i = 0; i < prevSquares.length; i++) {
+        const currentSquare = prevSquares[i]
+        if (currentSquare.id === id) {
+          const updatedSquare = {
+            ...currentSquare,
+            on: !currentSquare.on
+          }
+          newSquares.push(updatedSquare)
+        } else {
+          newSquares.push(currentSquare)
+        }
+      }
+      return newSquares
+    })
+  }
+
   const squareElements = squares.map(square => (
-    <Box key={square.id} on={square.on} />
+    <Box
+      key={square.id}
+      id={square.id}
+      on={square.on}
+      toggle={toggle}
+    />
   ))
 
   return (
